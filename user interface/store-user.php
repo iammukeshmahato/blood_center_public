0a75fc9e39e016803710c22411ee20fd8bb2ce58
Mukesh Mahato <mukeshmahato843@gmail.com>
2023-01-19 21:44:02 +0545

    Fix: inser query

--- user interface/store-user.php
+++ user interface/store-user.php
@@ -1,22 +1,16 @@
 <?PHP
 include('conn.php');
-if(isset($_POST['submit'])){
-	$first_name= $_POST['fname'];
-	$last_name= $_POST['lname'];
-	$email_address= $_POST['email'];
-	$phone_number= $_POST['phone_number'];
-	$password= $_POST['password'];
-	$profile_pic= $_POST['profile_picture'];
+if (isset($_POST['submit'])) {
+	// echo "php";
+	$first_name = $_POST['fname'];
+	$last_name = $_POST['lname'];
+	$email_address = $_POST['email'];
+	$phone_number = $_POST['phone_number'];
+	$password = $_POST['password'];
+	$profile_pic = $_POST['profile_picture'];
 
-	$sql="INSERT INTO user VALUES('$first_name','$last_name','$email_address', '$phone_number','$password','$profile_pic')";
-	$res=mysqli_query($conn,$sql);
-}
-else{
+	$sql = "INSERT INTO `user` (`first_name`, `last_name`, `email_address`, `phone_number`, `password`, `profile_pic`) VALUES ('$first_name','$last_name','$email_address', '$phone_number','$password','$profile_pic')";
+	$res = mysqli_query($conn, $sql);
+} else {
 	echo "NIASDJ";
 }
-// if (mysqli_query($conn, $sql)) {
-//         echo "New record created successfully";
-//     } else {
-//         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-//     }
-?>
