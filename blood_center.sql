33dcbd3501e503329fa54228fb2d2f54d5a70a30
Niruta Dahal <nirutadahal502@gmail.com>
2023-01-22 21:08:40 +0545

    adding db

--- blood_center.sql
+++ blood_center.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2023 at 04:31 PM
+-- Generation Time: Jan 22, 2023 at 04:21 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `donors_information`
+--
+
+CREATE TABLE `donors_information` (
+  `user` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact_number` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `weight` varchar(100) NOT NULL,
+  `age` varchar(100) NOT NULL,
+  `blood_group` varchar(100) NOT NULL,
+  `diseases` varchar(250) NOT NULL,
+  `blood_test_report` varchar(200) NOT NULL,
+  `rules` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -38,8 +59,22 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email_address`, `phone_number`, `password`, `profile_pic`) VALUES
+(7, 'Niruta', 'Dahal', 'nirutadahal502@gmail.com', '9861697303', 'nnn', '');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `donors_information`
+--
+ALTER TABLE `donors_information`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
 
 --
 -- Indexes for table `user`
@@ -52,10 +87,26 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `donors_information`
+--
+ALTER TABLE `donors_information`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `donors_information`
+--
+ALTER TABLE `donors_information`
+  ADD CONSTRAINT `donors_information_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
